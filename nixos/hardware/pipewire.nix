f4e0d4b1e0dad667cb7cb73f969e53960ee4de51
pyndys <mikumiku@mail>
2026-02-21 12:56:26 +0300

    edit pipewire

--- nixos/hardware/pipewire.nix
+++ nixos/hardware/pipewire.nix
@@ -1,13 +1,15 @@
 {
-  services.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
-
   security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    pulseaudio.enable = false;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
 }
