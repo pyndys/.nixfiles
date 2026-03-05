966d8cb9faaa626a14c1b0b70bed99f1bb19724f
pyndys <mikumiku@mail>
2026-03-05 17:24:14 +0300

    some

--- home/shell/lf.nix
+++ home/shell/lf.nix
@@ -1,5 +1,20 @@
-{
+{pkgs, ...}: {
   programs.lf = {
     enable = true;
+
+    settings = {
+      preview = true;
+    };
+
+    previewer = {
+      source = "${pkgs.ctpv}/bin/ctpv";
+    };
+
+    extraConfig = ''
+      set cleaner ${pkgs.ctpv}/bin/ctpvclear
+
+      &${pkgs.ctpv}/bin/ctpv -s $id
+      cmd on-quit %${pkgs.ctpv}/bin/ctpv -e $id
+    '';
   };
 }
