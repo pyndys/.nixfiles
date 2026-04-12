8f332642cf90468dc044a7092d26774c54a03905
pyndys <mikumiku@mail>
2026-04-12 21:17:44 +0300

    update hosts

--- nixos/network/hosts.nix
+++ nixos/network/hosts.nix
@@ -9,7 +9,7 @@
       pkgs.runCommand "filtered-hosts" {} ''
         ${pkgs.gnused}/bin/sed '/api\.github\.com/d' ${pkgs.fetchurl {
           url = "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/master/hosts";
-          sha256 = "sha256-rJ8ngiPJVWL3+BxtdoJMkg57J5Y/CdY7+GitDU5HrWg=";
+          sha256 = "sha256-jS/nBlsCTd/M4SDVWIUwJ8/bIUc+nMuIvBNIWo+3/Yo=";
         }} > $out
       ''
     );
