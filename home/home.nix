3483d28ea81b86ba497e1a773faef8a3c3060bb5
pyndys <mikumiku@mail>
2026-05-26 00:38:11 +0300

    edit home-manager

--- home/home.nix
+++ home/home.nix
@@ -9,11 +9,12 @@
   home = {
     username = "pyndys";
     homeDirectory = "/home/pyndys";
-    stateVersion = "25.05";
+    stateVersion = "26.05";
     sessionVariables = {
       EDITOR = "nvim";
       NIXOS_OZONE_WL = "1";
     };
     shell.enableFishIntegration = true; # Fish everywhere!!!
+    enableNixpkgsReleaseCheck = false;
   };
 }
