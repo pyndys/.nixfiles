e680ba56fe71a5ab83571c5d39f159c4a91891f5
pyndys <mikumiku@mail>
2026-05-28 15:03:57 +0300

    add placheloder secret

--- nixos/network/tg-ws-proxy.nix
+++ nixos/network/tg-ws-proxy.nix
@@ -12,7 +12,7 @@
     wantedBy = ["multi-user.target"];
     after = ["network.target"];
     serviceConfig = {
-      ExecStart = "${inputs.tg-ws-proxy.packages.${pkgs.system}.default}/bin/tg-ws-proxy --port 8443 --fake-tls-domain 4pda.to";
+      ExecStart = "${inputs.tg-ws-proxy.packages.${pkgs.system}.default}/bin/tg-ws-proxy --port 8443 --fake-tls-domain 4pda.to --secret 11111111111111111111111111111111";
       Restart = "on-failure";
       DynamicUser = true;
     };
