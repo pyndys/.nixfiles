35862929a03fc17815650a6ec0a1f47999e41a45
pyndys <mikumiku@beam>
2026-08-20 22:07:33 +0300

    edit matugen

--- home/matugen/default.nix
+++ home/matugen/default.nix
@@ -1,7 +1,4 @@
-{ config, inputs, ... }:
-let
-  templates = "${config.home.homeDirectory}/.nixfiles/home/matugen/templates";
-in
+{ inputs, ... }:
 {
   imports = [
     inputs.matugenix.homeModules.default
@@ -15,7 +12,7 @@ in
       equibop.themeVariant = "dms";
     };
     settings.templates.telegram = {
-      input_path = "${templates}/telegram.tdesktop-theme";
+      input_path = ./templates/telegram.tdesktop-theme;
       output_path = "~/Documents/matugen.tdesktop-theme";
     };
   };
